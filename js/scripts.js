0d202621dec55de0da74e0a147fe4e4dac361df5
Jim Patt <wrench1138@gmail.com>
2021-06-25 01:54:29 -0700

    img files added

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,43 @@ $( document ).ready(function() {
   var headerHeight = $('#header').height();
 
   $('.menu-item:first-child').click(function() {
-    $('#copy-container').slideUp();
+    $('#copy-container').slideUp('fast');
     window.scroll(0, 0);
   });
 
-  $('.menu-item:not(:first-child)').click(function() {
-    $('#copy-container').slideDown();
-    $('#copy-container').css('display', 'inline-block');
-console.log(document.body.scrollTop);
 
-//console.log(event.target.id);
-//    switch (event.target.id) {
-//      case "menu-about":
-//        $('#about-copy').fadeIn();
-//        break;
-//      case "menu-services":
-//        $('#services-copy').fadeIn();
-//        break;
-//    }
-//    window.scroll(0, headerHeight);
+  $('.menu-item:not(:first-child)').click(function() {
+    $('#copy-container').css('display', 'inline-block');
+    $('#copy-container > div').hide();
+    $('#copy-container').slideDown('slow', function() {
+
+
+      switch (event.target.id) {
+        case "menu-about":
+          $('#about-copy').fadeIn();
+          break;
+        case "menu-services":
+          $('#services-copy').fadeIn();
+          break;
+        case "menu-pack":
+          $('#pack-copy').fadeIn();
+          break;
+        case "menu-contact":
+          $('#contact-copy').fadeIn();
+          break;
+      }
+    });
+
 
   });
 
 
-
+  $('#carousel-button-left').click(function() {
+    $('#carousel-slide').css('left', '-500px');
+  });
+  $('#carousel-button-right').click(function() {
+    $('#carousel-slide').css('left', '0px');
+  });
 
 
 });
